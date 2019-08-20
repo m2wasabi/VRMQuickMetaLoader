07422f4cbe892007ff125c05d614f65a2c610d9b
m2wasabi <m2wasabi@gmail.com>
2019-08-20 15:37:00 +0900

    Demo 2

---
+++ Assets/VRM.QuickMetaLoader/Example/Scripts/CharacterCard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using VRM;
+
+public class CharacterCard : MonoBehaviour
+{
+    [SerializeField] private Text title;
+    [SerializeField] private Text version;
+    [SerializeField] private Text violence;
+    [SerializeField] private Text sexual;
+    [SerializeField] private Text commercial;
+    [SerializeField] private RawImage thumbnail;
+
+    public void SetMetaText(VRMMetaObject meta)
+    {
+        title.text = meta.Title;
+        version.text = meta.Version;
+        violence.text = meta.ViolentUssage.ToString();
+        sexual.text = meta.SexualUssage.ToString();
+        commercial.text = meta.CommercialUssage.ToString();
+    }
+
+    public void SetThumbnail(Texture2D tex)
+    {
+        thumbnail.texture = tex;
+    }
+}
---
+++ Assets/VRM.QuickMetaLoader/Example/Scripts/LoadManyMeta.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using VRM.QuickMetaLoader;
+
+public class LoadManyMeta : MonoBehaviour
+{
+    [SerializeField] private RectTransform _cardPrefab;
+
+    void Start()
+    {
+        var vrms = Directory.GetFiles (".", "*.vrm", System.IO.SearchOption.TopDirectoryOnly);
+
+        foreach (var vrm in vrms)
+        {
+            System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+            stopwatch.Start();
+            LoadMeta(vrm);
+            stopwatch.Stop();
+            Debug.Log("LoadTime: " + (float)stopwatch.Elapsed.TotalSeconds + " sec");
+        }
+
+    }
+
+    private void LoadMeta(string file)
+    {
+        var bytes = File.ReadAllBytes(file);
+        var metaLoader = new MetaLoader(bytes);
+        var meta = metaLoader.Read();
+
+        var go = Instantiate(_cardPrefab, transform);
+        var card = go.GetComponent<CharacterCard>();
+        card.SetMetaText(meta);
+        LoadThumbnail(card, metaLoader);
+
+    }
+
+    private void LoadThumbnail(CharacterCard card ,MetaLoader metaLoader)
+    {
+        var t = metaLoader.LoadAsyncThumbnail();
+        card.SetThumbnail(t);
+    }
+}
