df7e5554d3133e6f34aa62ac0527069ba1ce87a6
m2wasabi <m2wasabi@gmail.com>
2019-08-21 04:29:58 +0900

    Add Demos Coloutine and Async

---
+++ Assets/VRM.QuickMetaLoader/Example/Scripts/LoadManyMetaAsync.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using UnityEngine;
+using VRM.QuickMetaLoader;
+
+public class LoadManyMetaAsync : MonoBehaviour
+{
+    [SerializeField] private RectTransform _cardPrefab;
+
+    void Start()
+    {
+        var vrms = Directory.GetFiles (".", "*.vrm", System.IO.SearchOption.TopDirectoryOnly);
+        foreach (var vrm in vrms)
+        {
+            LoadMetaAsync(vrm);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            foreach (Transform child in transform)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var vrms = Directory.GetFiles (".", "*.vrm", System.IO.SearchOption.TopDirectoryOnly);
+            foreach (var vrm in vrms)
+            {
+                LoadMetaAsync(vrm);
+            }
+        }
+    }
+
+    private async void LoadMetaAsync(string file)
+    {
+        System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+        stopwatch.Start();
+        var bytes = await Task.Run(() => File.ReadAllBytes(file));
+        var metaLoader = new MetaLoader(bytes);
+        var meta = await metaLoader.ReadAsync(true);
+        stopwatch.Stop();
+        Debug.Log("LoadTime: " + (float)stopwatch.Elapsed.TotalSeconds + " sec");
+
+        var go = Instantiate(_cardPrefab, transform);
+        var card = go.GetComponent<CharacterCard>();
+        card.SetMetaText(meta);
+        card.SetThumbnail(meta.Thumbnail);
+    }
+}
---
+++ Assets/VRM.QuickMetaLoader/Example/Scripts/LoadManyMetaColoutine.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using VRM.QuickMetaLoader;
+
+public class LoadManyMetaColoutine : MonoBehaviour
+{
+    [SerializeField] private RectTransform _cardPrefab;
+
+    void Start()
+    {
+        var vrms = Directory.GetFiles (".", "*.vrm", System.IO.SearchOption.TopDirectoryOnly);
+        StartCoroutine(LoadVrmColoutine(vrms));
+
+    }
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            foreach (Transform child in transform)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var vrms = Directory.GetFiles (".", "*.vrm", System.IO.SearchOption.TopDirectoryOnly);
+            StartCoroutine(LoadVrmColoutine(vrms));
+        }
+    }
+
+    private void LoadMeta(string file)
+    {
+        System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+        stopwatch.Start();
+        var bytes = File.ReadAllBytes(file);
+        var metaLoader = new MetaLoader(bytes);
+        var meta = metaLoader.Read(true);
+        stopwatch.Stop();
+        Debug.Log("LoadTime: " + (float)stopwatch.Elapsed.TotalSeconds + " sec");
+
+        var go = Instantiate(_cardPrefab, transform);
+        var card = go.GetComponent<CharacterCard>();
+        card.SetMetaText(meta);
+        card.SetThumbnail(meta.Thumbnail);
+    }
+
+    private IEnumerator LoadVrmColoutine(string[] files)
+    {
+        foreach (var file in files)
+        {
+            LoadMeta(file);
+            yield return null;
+        }
+    }
+}
