6764c1288f7f39687d65bcfdc21d325bebef5f23
m2wasabi <m2wasabi@gmail.com>
2019-08-21 04:22:35 +0900

    Rolling cube for demo

---
+++ Assets/VRM.QuickMetaLoader/Example/Scripts/Rolling.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rolling : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(Vector3.up, 5.0f);
+    }
+}
